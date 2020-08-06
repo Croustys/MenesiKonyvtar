9334e1323a27b29aa010eae8ee00603a5ce7a20d
Croustys <51267148+Croustys@users.noreply.github.com>
2020-08-06 14:13:28 +0200

    backend update

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || 'mongodb://dbCroAdmin:dbCroAdmin@ds251284.mlab.com:51284/heroku_nnfdvdmk'
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
