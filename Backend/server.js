691ef3b3297516486495568f338f68a197fb0dfd
Croustys <51267148+Croustys@users.noreply.github.com>
2020-08-08 21:31:52 +0200

    a

--- Backend/server.js
+++ Backend/server.js
@@ -32,15 +32,12 @@ app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/ids', idRouter);
 
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public', 'index.html'));
     })
-    app.get('/', (req, res) => {
-        res.sendFile('../public/index.html')
-    })
-}
+//}
 
 //app.set("port", port)
 app.listen(port, () => {
