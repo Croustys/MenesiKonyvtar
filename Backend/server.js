12be683cb563ea68c5ee0f46e91c1d8107e10dd5
Croustys <51267148+Croustys@users.noreply.github.com>
2020-08-08 21:09:43 +0200

    a

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/v1/ids', idRouter);
 
 //if (process.env.NODE_ENV === "production") {
 
-app.use(express.static('/build'));
+app.use(express.static('build'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 })
