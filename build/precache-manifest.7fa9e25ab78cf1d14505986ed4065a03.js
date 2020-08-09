a9e3edfbf2a4d3b8e422ec56127a87ad0e690e95
Croustys <51267148+Croustys@users.noreply.github.com>
2020-08-09 17:51:06 +0200

    na

---
+++ build/precache-manifest.7fa9e25ab78cf1d14505986ed4065a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515c5c897b313969922579e2f6a8e3a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0580fb1a4181ff23b41d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fbfddfef69817dd118c",
+    "url": "/static/css/main.e28507a9.chunk.css"
+  },
+  {
+    "revision": "0580fb1a4181ff23b41d",
+    "url": "/static/js/2.9433cf17.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.9433cf17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbfddfef69817dd118c",
+    "url": "/static/js/main.fcb94be1.chunk.js"
+  },
+  {
+    "revision": "a61dc00fc354b2d3d984",
+    "url": "/static/js/runtime-main.fc21bb0a.js"
+  }
+]);
